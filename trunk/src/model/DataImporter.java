1b687019cf6539d74f9c7f9f8bd4406dae0a7883
nshamgar <nshamgar@0e8638fa-d768-11dd-a8b6-f5aca7b582bb>
2009-01-22 17:31:43 +0000

    Temp classes for parsing.

---
+++ trunk/src/model/DataImporter.java
@@ -0,0 +1,66 @@
+package model;
+import java.io.*;
+import java.util.regex.*;
+import java.util.*;
+
+public class DataImporter {
+
+ public static void main(String argv[]) {
+	 
+	 // Searching for "LANGUAGE LIST" & "============="
+	 Parser languageParser = new Parser("language.list");
+	 String line;
+	 
+	 // Making sure we find the start of the language list
+	 if (languageParser.findLine("LANGUAGE LIST")) {
+		 if (languageParser.findLine("============="))
+		 {
+			 // defining a dictionary to find all the different languages
+			 Set languagesSet = new HashSet();  
+			 
+			 System.out.println("Found the start of the list!");
+			 //Pattern p = Pattern.compile("[^\"].+[)}]\\s+([a-zA-Z_\\-\\s',0-9]+)(\\s+\\(.+\\)\\s*)*");
+			 // Pattern explanation:
+			 // [^\"] 							- ignore lines that start with '"' (since they are TV shows)
+			 // .+[)}] 							- run along the line over all characters until you encounter ')' or '}'
+			 // \\s+ 							- run along all the trailing whitespaces
+			 // ([\\P{InGreek}\\-\\s',&&[^(]]+)	- group 1: run over all different UNICODE letters including '-', ',' (comma), ''' (single quote) and whitespaces
+			 // 											not including '('
+			 // (\\s+\\(.+\\)\\s*)* 			- check if this group appears zero or more times
+			 //										the group contains whitespaces, then '(', different characters and whitespaces, and then ')'
+			 //										(there can be more than one comments in parentheses)
+			 //Pattern p = Pattern.compile("[^\"].+[)}]\\s+([\\P{InGreek}\\-\\s',&&[^(]]+)(\\s+\\(.+\\)\\s*)*");
+			 Pattern p = Pattern.compile("[^\"].+[)}]\\s+([\\P{InGreek}\\-\\s',&&[^(]]+)(\\s+\\(.+\\)\\s*)*");
+			 int i = 15;
+			 while (!languageParser.isEOF()) {
+				 line = languageParser.getNextLine();
+				 //System.out.println(line);
+				 Matcher m = p.matcher(line);
+				 boolean b = m.matches();
+				 if (!b) {
+					 if (!(line.charAt(0) == '"')) {
+						 System.out.println(line);
+						 System.out.println("couldn't found a match on line " + i + "!");
+					 }
+				 }
+//				 System.out.println(m.group(1));
+				 else {
+					 languagesSet.add(m.group(1));
+//					 if (m.group(1).equals("Language")) {
+//						 System.out.println(line);
+//					 }
+//					 System.out.println(m.group(1));
+//					 System.out.println("couldn't found a match!");
+				 }
+				 i++;
+			 }
+			 for (Object language : languagesSet)
+				 System.out.println(language);
+			 
+		 }
+	 }
+	 
+	 languageParser.closeFile();
+
+ }
+}
